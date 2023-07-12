163c2ba29f3913fd69f8fa77b930179854ca2f95
David Chen <davidc3287@gmail.com>
2023-07-11 22:16:18 -0700

    splashscreen

--- src/app/page.js
+++ src/app/page.js
@@ -9,45 +9,22 @@ export default function Home() {
 
   return (
     <main>
-      <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-        Click me
-      </button>
-      <button
-        type="button"
-        class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
+      <div
+        className="h-screen max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 bg-cover"
+        style={{
+          backgroundImage: "url('/assets/splashscreen.png')",
+        }}
       >
-        Solid
-      </button>
-      <button
-        type="button"
-        class="py-[.688rem] px-4 inline-flex justify-center items-center gap-2 rounded-md border-2 border-gray-200 font-semibold text-blue-500 hover:text-white hover:bg-blue-500 hover:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:border-gray-700 dark:hover:border-blue-500"
-      >
-        Outline
-      </button>
-      <button
-        type="button"
-        class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold text-blue-500 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm"
-      >
-        Ghost
-      </button>
-      <button
-        type="button"
-        class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md bg-blue-100 border border-transparent font-semibold text-blue-500 hover:text-white hover:bg-blue-500 focus:outline-none focus:ring-2 ring-offset-white focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm"
-      >
-        Soft
-      </button>
-      <button
-        type="button"
-        class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
-      >
-        White
-      </button>
-      <button
-        type="button"
-        class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold text-blue-500 hover:text-blue-700 focus:outline-none focus:ring-2 ring-offset-white focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm"
-      >
-        Link
-      </button>
+        <div className="absolute top-1/2 left-1/2 text-center">
+          <h1 className="text-7xl text-white font-boldtransform -translate-x-1/2 -translate-y-1/2 font-semibold">
+            Socal Datathon
+          </h1>
+          <h2 className="text-5xl text-white -translate-x-1/2 -translate-y-1/2">
+            March 3, 2024
+          </h2>
+        </div>
+      </div>
+      <div>hi</div>
     </main>
   );
 }
